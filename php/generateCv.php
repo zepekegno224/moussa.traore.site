e4600de75470df78ffb01a5442e0a86231e09b9f
Moussa Traore <moussatraore158@gmail.com>
2023-09-02 01:58:41 +0000

    Ajout de la generation de pdf

--- php/generateCv.php
+++ php/generateCv.php
@@ -5,7 +5,7 @@ use Dompdf\Dompdf;
 require './vendor/autoload.php';
 
 $dompdf = new Dompdf();
-$dompdf->loadHtml('resume.html');
+$dompdf->loadHtml('./resume.html');
 $dompdf->setPaper('A4', 'portrait');
 $dompdf->render();
 
